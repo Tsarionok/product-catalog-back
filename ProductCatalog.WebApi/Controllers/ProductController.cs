f381b45eb463e6e12d01ec720167ce49f66f50de
Sergey Tsarionok <tsarionok@gmail.com>
2023-09-27 18:47:14 +0300

    add delete controller method

--- ProductCatalog.WebApi/Controllers/ProductController.cs
+++ ProductCatalog.WebApi/Controllers/ProductController.cs
@@ -10,33 +10,40 @@ namespace ProductCatalog.WebApi.Controllers;
 [Route("api/[controller]")]
 public class ProductController : ControllerBase
 {
-    private readonly IProductService _service;
+    private readonly IProductService _productService;
     private readonly IMapper _mapper;
 
-    public ProductController(IProductService service, IMapper mapper)
+    public ProductController(IProductService productService, IMapper mapper)
     {
-        _service = service;
+        _productService = productService;
         _mapper = mapper;
     }
     
     [HttpGet("{id:long}")]
     public async Task<ProductApiModel> GetAsync(long id)
     {
-        var productResult = await _service.GetByIdAsync(id);
+        var productResult = await _productService.GetByIdAsync(id);
         return _mapper.Map<ProductApiModel>(productResult);
     }
 
     [HttpGet]
     public async Task<IEnumerable<ProductApiModel>> GetAsync()
     {
-        var productsResult = await _service.GetAllAsync();
+        var productsResult = await _productService.GetAllAsync();
         return _mapper.Map<IEnumerable<ProductApiModel>>(productsResult);
     }
 
     [HttpPost]
     public async Task<ProductApiModel> AddAsync(AddProductApiModel product)
     {
-        var productResult = await _service.AddAsync(_mapper.Map<AddProductDto>(product));
+        var productResult = await _productService.AddAsync(_mapper.Map<AddProductDto>(product));
         return _mapper.Map<ProductApiModel>(productResult);
+    }
+
+    [HttpDelete]
+    public async Task<ProductApiModel> DeleteAsync(long id)
+    {
+        var deletedProduct = await _productService.DeleteByIdAsync(id);
+        return _mapper.Map<ProductApiModel>(deletedProduct);
     }
 }
