2c49138c24ed955e12dfdc3499082046ca5b7851
Sergey Tsarionok <tsarionok@gmail.com>
2023-09-28 23:36:51 +0300

    DB entities experiments

--- ProductCatalog.DataAccess/Entities/Category.cs
+++ ProductCatalog.DataAccess/Entities/Category.cs
@@ -5,6 +5,6 @@ namespace ProductCatalog.DataAccess.Entities;
 public class Category : BaseModel<long>
 {
     public string Name { get; set; }
-
+    
     public ICollection<Product> Products { get; set; }
 }
---
+++ ProductCatalog.DataAccess/Migrations/20230928203046_RevertOneToOneMigration.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductCatalog.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class RevertOneToOneMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_products_CategoryId",
+                table: "products");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "products",
+                newName: "category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_category_id",
+                table: "products",
+                column: "category_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_categories_category_id",
+                table: "products",
+                column: "category_id",
+                principalTable: "categories",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_categories_category_id",
+                table: "products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_products_category_id",
+                table: "products");
+
+            migrationBuilder.RenameColumn(
+                name: "category_id",
+                table: "products",
+                newName: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_CategoryId",
+                table: "products",
+                column: "CategoryId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products",
+                column: "CategoryId",
+                principalTable: "categories",
+                principalColumn: "id");
+        }
+    }
+}
